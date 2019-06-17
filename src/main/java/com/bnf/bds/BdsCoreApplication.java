30d7068840567068bba771ae39dac19f65a2e01d
TY <TY@DESKTOP-ET7O2E7>
2019-06-17 14:15:59 +0900

    test

---
+++ src/main/java/com/bnf/bds/BdsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.bnf.bds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdsCoreApplication.class, args);
+	}
+
+}
